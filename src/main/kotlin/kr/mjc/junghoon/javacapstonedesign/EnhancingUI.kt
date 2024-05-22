5b0a0f41b1a4caa5344f1ed653ee2234d703586e
junghoon9 <165883618+junghoon9@users.noreply.github.com>
2024-05-22 12:35:52 +0900

    Update EnhancingUI.kt

--- src/main/kotlin/kr/mjc/junghoon/javacapstonedesign/EnhancingUI.kt
+++ src/main/kotlin/kr/mjc/junghoon/javacapstonedesign/EnhancingUI.kt
@@ -3,19 +3,23 @@ package kr.mjc.junghoon.javacapstonedesign
 import javax.swing.*
 import java.awt.*
 
-class EnhancingUI(private val itemType: String?, private val jobType: String?, private val itemLevel: Int?) : JFrame() {
-    val startButton = JButton("강화 시작")
-    val endButton = JButton("강화 종료")
+class EnhancingUI(private val itemType: String?,
+                  private val jobType: String?,
+                  private val itemLevel: Int?,
+                  private val logic: Logic) : JFrame() {
+    private val startButton = JButton("강화 시작")
+    private val endButton = JButton("강화 종료")
+    private val miniGameCheckBox = JCheckBox("미니게임 실행")
     private var enhancingInfoLabel = JLabel()
+
     private fun updateEnhancingInfo(logic: Logic) {
         // logic 객체에서 강화 정보 가져오기
-        val ehcLevel = logic.getEnhancingLevel()
-        val statN = logic.statName()
-        val statInc = logic.getStatIncrease()
-        val powerN = logic.powerName()
-        val powerInc = logic.getPowerIncrease()
-        val defN = logic.defName()
-        val defInc = logic.getDefIncrease()
+        val ehcLevel = logic.enhancingLevel
+        val statN = logic.statName
+        val statInc = logic.statIncrease
+        val powerOrDefenceN = logic.powerOrDefenceName
+        val powerInc = logic.powerIncrease
+        val defInc = logic.defIncrease
         val sChance = logic.getSuccessChance()
         val fChance = logic.getFailureChance()
         val cost = logic.getCost()
@@ -26,17 +30,17 @@ class EnhancingUI(private val itemType: String?, private val jobType: String?, p
                 "<html>" +
                 "$ehcLevel 강 -> ${ehcLevel + 1} 강<br><br>" +
                 "$statN: +$statInc<br>" +
-                "$powerN: +$powerInc<br><br>" +
+                "$powerOrDefenceN: +$powerInc<br><br>" +
                 "성공확률: $sChance %<br>" +
                 "실패확률: $fChance %<br><br>" +
                 "강화비용: $cost 골드<br>" +
                 "</html>"
             }
-            else -> { // 방어구
+            else -> { //방어구
                 "<html>" +
                 "$ehcLevel 강 -> ${ehcLevel + 1} 강<br><br>" +
-                "statN: +$statInc<br>" +
-                "$defN: +$defInc<br><br>" +
+                "$statN: +$statInc<br>" +
+                "$powerOrDefenceN: +$defInc<br><br>" +
                 "성공확률: $sChance %<br>" +
                 "실패확률: $fChance %<br><br>" +
                 "강화비용: $cost 골드<br>" +
@@ -45,14 +49,21 @@ class EnhancingUI(private val itemType: String?, private val jobType: String?, p
         }
 
         // 강화 정보 레이블 업데이트
-        enhancingInfoLabel = JLabel(enhancingInfoText)
+        enhancingInfoLabel.text = enhancingInfoText
+
+        // 최대 강화차수에 도달했는지 확인
+        if (logic.enhancingLevel >= logic.maxEnhancingLevel) {
+            val finishUI = FinishUI(logic)
+            finishUI.isVisible = true
+            this@EnhancingUI.dispose() // 현재 창 닫기
+            return
+        }
     }
 
 
-    init {
-        val titleText = "아이템 강화"
 
-        title = titleText
+    init {
+        title = "아이템 강화"
         defaultCloseOperation = EXIT_ON_CLOSE
         setSize(800, 600)
         setLocationRelativeTo(null)
@@ -73,13 +84,13 @@ class EnhancingUI(private val itemType: String?, private val jobType: String?, p
                 "전사" -> "/images/weapon/warrior_weapon.png"
                 "궁수" -> "/images/weapon/archer_weapon.png"
                 "마법사" -> "/images/weapon/magician_weapon.png"
-                else -> "/images/weapon/thief_weapon.png"
+                else -> "/images/weapon/thief_weapon.png" //도적
             }
             else -> when (jobType) {
                 "전사" -> "/images/armor/warrior_armor.png"
                 "궁수" -> "/images/armor/archer_armor.png"
                 "마법사" -> "/images/armor/magician_armor.png"
-                else -> "/images/armor/thief_armor.png"
+                else -> "/images/armor/thief_armor.png" //도적
             }
         }
         val imageURL = javaClass.getResource(imagePath)
@@ -97,14 +108,12 @@ class EnhancingUI(private val itemType: String?, private val jobType: String?, p
 
         // 우측에 강화 정보를 표시할 빈 영역
         val enhancingInfoPanel = JPanel()
-        val logic = Logic(itemType, jobType, itemLevel)
-        val ehcLevel = logic.getEnhancingLevel()
-        val statN = logic.statName()
-        val statInc = logic.getStatIncrease()
-        val powerN = logic.powerName()
-        val powerInc = logic.getPowerIncrease()
-        val defN = logic.defName()
-        val defInc = logic.getDefIncrease()
+        val ehcLevel = logic.enhancingLevel
+        val statN = logic.statName
+        val statInc = logic.statIncrease
+        val powerOrDefenceN = logic.powerOrDefenceName
+        val powerInc = logic.powerIncrease
+        val defInc = logic.defIncrease
         val sChance = logic.getSuccessChance()
         val fChance = logic.getFailureChance()
         val cost = logic.getCost()
@@ -113,7 +122,7 @@ class EnhancingUI(private val itemType: String?, private val jobType: String?, p
                 "<html>" +
                 "$ehcLevel 강 -> ${ehcLevel + 1} 강<br><br>" +
                 "$statN: +$statInc<br>" +
-                "$powerN: +$powerInc<br><br>" +
+                "$powerOrDefenceN: +$powerInc<br><br>" +
                 "성공확률: $sChance %<br>" +
                 "실패확률: $fChance %<br><br>" +
                 "강화비용: $cost 골드<br>" +
@@ -122,8 +131,8 @@ class EnhancingUI(private val itemType: String?, private val jobType: String?, p
             else -> { // 방어구
                 "<html>" +
                 "$ehcLevel 강 -> ${ehcLevel + 1} 강<br><br>" +
-                "statN: +$statInc<br>" +
-                "$defN: +$defInc<br><br>" +
+                "$statN: +$statInc<br>" +
+                "$powerOrDefenceN: +$defInc<br><br>" +
                 "성공확률: $sChance %<br>" +
                 "실패확률: $fChance %<br><br>" +
                 "강화비용: $cost 골드<br>" +
@@ -135,21 +144,35 @@ class EnhancingUI(private val itemType: String?, private val jobType: String?, p
         enhancingInfoLabel.font = font
 
         enhancingInfoPanel.add(enhancingInfoLabel)
+        enhancingInfoPanel.add(miniGameCheckBox, BorderLayout.SOUTH)
 
 
         // 하단에 강화 시작 버튼과 강화 종료 버튼을 추가할 패널
         val buttonPanel = JPanel()
-        buttonPanel.add(startButton)
 
-        startButton.addActionListener {
+        // 강화 시작 버튼
+        buttonPanel.add(startButton)
+        startButton.addActionListener{
             if (itemType != null && jobType != null && itemLevel != null) {
+                if (miniGameCheckBox.isSelected) {
+                    val miniGame = MiniGame()
+                    miniGame.isVisible = true
+                    logic.updateSuccessChance()
+                }
                 logic.simulateItem()
                 this@EnhancingUI.updateEnhancingInfo(logic)
             }
         }
 
-        buttonPanel.add(startButton)
+        // 강화 종료 버튼
         buttonPanel.add(endButton)
+        endButton.addActionListener{
+            if (itemType != null && jobType != null && itemLevel != null) {
+                val finishUI = FinishUI(logic)
+                finishUI.isVisible = true
+                isVisible = false
+            }
+        }
 
         add(imagePanel, BorderLayout.WEST)
         add(enhancingInfoPanel, BorderLayout.CENTER)
@@ -163,12 +186,13 @@ fun main() {
         itemSelectingUI.isVisible = false
 
         // ItemSelectingUI 클래스에서 선택한 아이템 종류와 직업군을 가져옴
-        val it = itemSelectingUI.getSelectedItemType()
-        val jt = itemSelectingUI.getSelectedJobType()
-        val il = itemSelectingUI.getSelectedLevel()
+        val it = itemSelectingUI.selectedItemType
+        val jt = itemSelectingUI.selectedJobType
+        val il = itemSelectingUI.selectedLevel
+        val lg = Logic(it, jt, il)
 
         // EnhancingUI 클래스의 인스턴스를 생성할 때 선택한 아이템 종류와 직업군을 전달
-        val enhancingUI = EnhancingUI(it, jt, il)
+        val enhancingUI = EnhancingUI(it, jt, il, lg)
         enhancingUI.isVisible = true
     }
 }
