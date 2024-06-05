0302bab1ee019a65fca452a180beccdfb463089a
junghoon9 <165883618+junghoon9@users.noreply.github.com>
2024-06-05 16:53:58 +0900

    Update Start_afterLogin.kt

--- src/main/kotlin/kr/mjc/junghoon/javacapstonedesign/main/Start_afterLogin.kt
+++ src/main/kotlin/kr/mjc/junghoon/javacapstonedesign/main/Start_afterLogin.kt
@@ -1,12 +1,18 @@
 package kr.mjc.junghoon.javacapstonedesign.main
 
-import javax.swing.*
-import java.awt.*
+import kr.mjc.junghoon.javacapstonedesign.login.LoginUI
+import kr.mjc.junghoon.javacapstonedesign.login.UserManager
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
+import java.awt.Insets
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
-import kotlin.system.exitProcess
+import javax.swing.*
 
-class StartAfterLoginUI : JFrame(), ActionListener {
+class StartAfterLoginUI(private var loggerInUserID: String?) : JFrame(), ActionListener {
+    private var welcomeLabel: JLabel
+    private val userManager = UserManager()
+
     init {
         title = "아이템 강화"
         defaultCloseOperation = EXIT_ON_CLOSE
@@ -20,49 +26,71 @@ class StartAfterLoginUI : JFrame(), ActionListener {
         val titleConstraints = GridBagConstraints().apply {
             gridx = 0
             gridy = 0
-            insets = Insets(20, 0, 20, 0)
+            insets = Insets(20, 0, 10, 0)
         }
         add(titleLabel, titleConstraints)
+
+
+
+        welcomeLabel = JLabel("환영합니다, ${getUsernameByID(loggerInUserID)} 님!")
+        welcomeLabel.horizontalAlignment = SwingConstants.CENTER
+        val welcomeConstraints = GridBagConstraints().apply {
+            gridx = 0
+            gridy = 1
+            insets = Insets(0, 0, 20, 0)
+        }
+        add(welcomeLabel, welcomeConstraints)
 
         val itemSetUpButton = JButton("아이템 설정")
         itemSetUpButton.addActionListener(this)
         val itemSetupConstraints = GridBagConstraints().apply {
             gridx = 0
-            gridy = 1
-            insets = Insets(10, 0, 10, 0)
+            gridy = 2
+            insets = Insets(20, 0, 10, 0)
             ipadx = 20
         }
         add(itemSetUpButton, itemSetupConstraints)
 
-        val exitButton = JButton("종료")
-        exitButton.addActionListener(this)
-        val exitConstraints = GridBagConstraints().apply {
+        val logoutButton = JButton("로그아웃")
+        logoutButton.addActionListener(this)
+        val logoutConstraints = GridBagConstraints().apply {
             gridx = 0
-            gridy = 2
+            gridy = 3
             insets = Insets(10, 0, 20, 0)
-            ipadx = 60
+            ipadx = 35
         }
-        add(exitButton, exitConstraints)
+        add(logoutButton, logoutConstraints)
+    }
+
+    private fun getUsernameByID(userID: String?): String {
+        return if (userID != null) {
+            userManager.getUsernameByUserID(userID)
+        }
+        else ""
     }
 
     override fun actionPerformed(e: ActionEvent?) {
         when (e?.actionCommand) {
             "아이템 설정" -> {
-                val itemSelectingUI = ItemSelectingUI()
-                itemSelectingUI.isVisible = true
-                isVisible = false // 현재 화면을 숨김
+                if (loggerInUserID != null) {
+                    SwingUtilities.invokeLater {
+                        val itemSelectingUI = ItemSelectingUI(loggerInUserID!!)
+                        itemSelectingUI.isVisible = true
+                    }
+                    isVisible = false // 현재 화면을 숨김
+                }
+                else JOptionPane.showMessageDialog(this, "로그인이 필요합니다.", "로그인 필요", JOptionPane.WARNING_MESSAGE)
             }
-            "종료" -> {
-                exitProcess(0)
+            "로그아웃" -> {
+                loggerInUserID = null
+                welcomeLabel.text = ""
+                JOptionPane.showMessageDialog(this, "로그아웃 되었습니다.", "로그아웃", JOptionPane.INFORMATION_MESSAGE)
+                SwingUtilities.invokeLater {
+                    val loginUI = LoginUI()
+                    loginUI.isVisible = true
+                }
+                isVisible = false
             }
         }
-    }
-}
-
-
-fun main() {
-    SwingUtilities.invokeLater {
-        val startAfterLoginUI = StartAfterLoginUI()
-        startAfterLoginUI.isVisible = true
     }
 }
